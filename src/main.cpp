d9636d60fc20eb39f2537a969f7e78ab58a3beec
PEDRO LEONEL DE LORENA <53955089886@info.local>
2026-05-27 15:03:03 -0300

    Versão 0.0.3: Adicionado as variaveis para controlar o tempo e o pause

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
  *  Descrição: Neste modulo de publish nos coletamos os hex do controle remoto e fizemos com que toda vez que apertasse o botao ele emitsse para a tela oq o hex faz.
  *  Projeto: Modulo de publicar os comandos feito na tela retratil
  *  Data: 21/05
- *  Versão: 0.0.2
+ *  Versão: 0.0.3
  */
 
 #include <Arduino.h>
@@ -27,13 +27,19 @@ String hexUP = "codigo do hexUP";
 String hexDOWN = "codigo do hexDOWN";
 String hexSELECT = "codigo do hexSELECT";
 
+const unsigned long tempoInicio = 0;
 
+unsigned long tempo = millis();
+unsigned long tempoPause;
 
+unsigned long tempoMaximo = 20000;
+
+bool tempoParado = false;
 //=========================
 
 //=========================
-//* CRIAÇÃO DE OBJETOS 
-  
+//* CRIAÇÃO DE OBJETOS
+
 Bounce UP = Bounce();
 Bounce DOWN = Bounce();
 Bounce SELECT = Bounce();
@@ -41,14 +47,12 @@ Bounce SELECT = Bounce();
 
 //=========================
 //* INVOCAR AS FUNÇÕES
- 
-bool botaoUP();
-bool botaoDOWN();
-bool botaoSELECT();
+
+void botaoUP();
+void botaoDOWN();
+void botaoSELECT();
 
 //=========================
-
-
 
 void setup()
 {
@@ -71,31 +75,33 @@ void loop()
   garantirMQTTConectado();
   loopMQTT();
 
-  if(UP.fell())
+  if (UP.fell())
   {
     botaoUP();
   }
 }
 
-bool botaoUP()
-{ 
-  if(hexUP == "")
-    debugErro("O código do hex está errado, favor checar se esta correto");
- 
-
-if(hexUP)
+void botaoUP()
 {
-  debugInfo("Tela retrátil subindo");
-  //TODO VER QUANTO TEMPO ELA DEMORA PRA SUBIR POR COMPLETO E FAZER UM CRONOMETTRO Q QUANDO ELA SUBIR TUDO VAI EXIVIR
-  //TODO FAZER UMA VARIAVEL DE PAUSE Q SALVA AONDE O MILLIS PAROU POIS QND ELE APERTAR O PAUSE O MILLIS NAO PODE CONTINUAR ANDANDO 
-  if(millis() - 0 >= 5000 ) // trocar esse 5000 dps
-  debugInfo("Tela retrátil subiu por completo.");
+  if (hexUP == "")
+    debugErro("O código do hex está errado, favor checar se esta correto");
 
+  if (hexUP == "codigo do hexUP")
+  {
+    // TODO VER QUANTO TEMPO ELA DEMORA PRA SUBIR POR COMPLETO E FAZER UM CRONOMETTRO Q QUANDO ELA SUBIR TUDO VAI EXIVIR
+    // TODO FAZER UMA VARIAVEL DE PAUSE Q SALVA AONDE O MILLIS PAROU POIS QND ELE APERTAR O PAUSE O MILLIS NAO PODE CONTINUAR ANDANDO
+    if (tempoParado)
+    {
+      debugInfo("Tela retrátil subindo");
 
-  String texto;
-  serializeJson(doc, texto);  
-  publicarMensagem(TOPICO_COMANDO, texto.c_str()); // ver como faz pra trocar o topico_comando pelo topico_publicar
+      if (tempo - tempoInicio >= tempoMaximo)
+      {
+        debugInfo("Tela retrátil subiu por completo.");
+      }
+    }
 
-
-}
+    String texto;
+    serializeJson(doc, texto);
+    publicarMensagem(TOPICO_COMANDO, texto.c_str()); // ver como faz pra trocar o topico_comando pelo topico_publicar
+  }
 }
